d5cc164fcf8197bb51b38884b1255fb3ed0ded87
zach <zachcelona@gmail.com>
2025-08-21 12:53:49 +1200

    w

---
+++ Backend/ChatbotBackend/Migrations/20250820134411_InitialCreate.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChatbotBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    FullName = table.Column<string>(type: "TEXT", nullable: false),
+                    Password = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    CalendarItems = table.Column<string>(type: "TEXT", nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    Gender = table.Column<string>(type: "TEXT", nullable: true),
+                    FamilyHistoryDementia = table.Column<string>(type: "TEXT", nullable: true),
+                    LifestyleHabits = table.Column<string>(type: "TEXT", nullable: true),
+                    MedicalConditions = table.Column<string>(type: "TEXT", nullable: true),
+                    PrimaryBrainHealthConcern = table.Column<string>(type: "TEXT", nullable: true),
+                    PreferredCommunicationStyle = table.Column<string>(type: "TEXT", nullable: true),
+                    CurrentMedications = table.Column<string>(type: "TEXT", nullable: true),
+                    SleepPatterns = table.Column<string>(type: "TEXT", nullable: true),
+                    StressLevels = table.Column<string>(type: "TEXT", nullable: true),
+                    CognitiveActivityPreference = table.Column<string>(type: "TEXT", nullable: true),
+                    LastKnownLocation = table.Column<string>(type: "TEXT", nullable: true),
+                    EmergencyContact = table.Column<string>(type: "TEXT", nullable: true),
+                    LastInteractionDate = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    CurrentAssessmentQuestionIndex = table.Column<int>(type: "INTEGER", nullable: false),
+                    AssessmentCompleted = table.Column<bool>(type: "INTEGER", nullable: false),
+                    AssessementStarted = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Calendar",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    EventName = table.Column<string>(type: "TEXT", nullable: false),
+                    EventDescription = table.Column<string>(type: "TEXT", nullable: false),
+                    EventDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    StartTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    EndTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Category = table.Column<string>(type: "TEXT", nullable: false),
+                    UserId = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Calendar", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Calendar_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ScoreResult",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Question = table.Column<string>(type: "TEXT", nullable: false),
+                    UserAnswer = table.Column<string>(type: "TEXT", nullable: false),
+                    Correct = table.Column<bool>(type: "INTEGER", nullable: false),
+                    ResponseTime = table.Column<TimeSpan>(type: "TEXT", nullable: false),
+                    Score = table.Column<int>(type: "INTEGER", nullable: false),
+                    UserId = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ScoreResult", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ScoreResult_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Calendar_UserId",
+                table: "Calendar",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ScoreResult_UserId",
+                table: "ScoreResult",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Calendar");
+
+            migrationBuilder.DropTable(
+                name: "ScoreResult");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ Backend/ChatbotBackend/Migrations/20250820134744_UpdateUserForAssessment.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChatbotBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUserForAssessment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ScoreResult");
+
+            migrationBuilder.CreateTable(
+                name: "QuestionResult",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Question = table.Column<string>(type: "TEXT", nullable: false),
+                    UserAnswer = table.Column<string>(type: "TEXT", nullable: false),
+                    Correct = table.Column<bool>(type: "INTEGER", nullable: false),
+                    ResponseTime = table.Column<TimeSpan>(type: "TEXT", nullable: false),
+                    UserId = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QuestionResult", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_QuestionResult_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QuestionResult_UserId",
+                table: "QuestionResult",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "QuestionResult");
+
+            migrationBuilder.CreateTable(
+                name: "ScoreResult",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Correct = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Question = table.Column<string>(type: "TEXT", nullable: false),
+                    ResponseTime = table.Column<TimeSpan>(type: "TEXT", nullable: false),
+                    Score = table.Column<int>(type: "INTEGER", nullable: false),
+                    UserAnswer = table.Column<string>(type: "TEXT", nullable: false),
+                    UserId = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ScoreResult", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ScoreResult_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ScoreResult_UserId",
+                table: "ScoreResult",
+                column: "UserId");
+        }
+    }
+}
